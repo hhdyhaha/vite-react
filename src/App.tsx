8b03a85daa104a1bc9b4162de71472bab5c898f9
hhdyhaha <1164549585@qq.com>
2024-04-25 21:47:53 +0800

    props传参

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Img} from "./views/ImgData.tsx";
 
+const imgUrl1 = "https://th.bing.com/th/id/R.98415963b412fccea2d8c4e2b3f695e8?rik=HdwmF8f2XbTwUQ&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2f25359e94f3481702ce699cc867b236c9d056adbc.jpg&ehk=aFN1%2f0CPFS8Bsm7lcpdWzXbRVVRniH9WE9GQ3nVnTNc%3d&risl=&pid=ImgRaw&r=0"
+
 function App() {
     return (
         // 如果多个节点，需要用Fragment包裹起来
         <>
-            <Img/>
+            <Img imgUrl={imgUrl1}/>
             <div>
                 haha
                 <hr />
--- src/views/ImgData.tsx
+++ src/views/ImgData.tsx
@@ -1,5 +1,4 @@
-export function Img() {
-    const imgUrl = "https://th.bing.com/th/id/R.98415963b412fccea2d8c4e2b3f695e8?rik=HdwmF8f2XbTwUQ&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2f25359e94f3481702ce699cc867b236c9d056adbc.jpg&ehk=aFN1%2f0CPFS8Bsm7lcpdWzXbRVVRniH9WE9GQ3nVnTNc%3d&risl=&pid=ImgRaw&r=0"
+export function Img({imgUrl}: {imgUrl: string}) {
     const animeInfo = {
         name: '不良人',
         status: '待完结',
