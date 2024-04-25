1dd329ec4caa24a913386f221246725f98d7a84c
hhdyhaha <1164549585@qq.com>
2024-04-25 22:20:20 +0800

    条件渲染

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import {Img} from "./views/ImgData.tsx";
 
 const imgUrl1 = "https://th.bing.com/th/id/R.98415963b412fccea2d8c4e2b3f695e8?rik=HdwmF8f2XbTwUQ&riu=http%3a%2f%2fi1.hdslb.com%2fbfs%2farchive%2f25359e94f3481702ce699cc867b236c9d056adbc.jpg&ehk=aFN1%2f0CPFS8Bsm7lcpdWzXbRVVRniH9WE9GQ3nVnTNc%3d&risl=&pid=ImgRaw&r=0"
-
+const imgUrl2 = "https://puui.qpic.cn/vpic_cover/h3345zrkd0f/h3345zrkd0f_hz.jpg/1280"
+const isHide = false
 function App() {
     return (
         // 如果多个节点，需要用Fragment包裹起来
         <>
-            <Img imgUrl={imgUrl1}/>
+            {/*分别传递三个参数，而不是包裹成对象*/}
+            <Img imgUrl1={imgUrl1} imgUrl2={imgUrl2} isHide={isHide} />
             <div>
                 haha
                 <hr />
--- src/views/ImgData.tsx
+++ src/views/ImgData.tsx
@@ -1,16 +1,25 @@
-export function Img({imgUrl}: {imgUrl: string}) {
+export function Img({ imgUrl1, imgUrl2, isHide }: { imgUrl1: string, imgUrl2: string, isHide: boolean }) {
     const animeInfo = {
         name: '不良人',
         status: '待完结',
-    }
+    };
+
+    // 条件判断应该在 JSX 之外
+    const imageElement = isHide ? (
+        <img src={imgUrl1} alt="这是一张图片" width={200}/>
+    ) : (
+        <img src={imgUrl2} alt="这是一张图片" width={200}/>
+    );
+
     return (
-        <>
-            <img src={imgUrl} alt="这是一张图片" width={200}/>
+        <div>
+            {imageElement}
             <div>
                 <h1>{animeInfo.name}</h1>
-                <p style={{color: 'red', fontSize: '20px'}}>{animeInfo.status}</p>
+                <p style={{ color: 'red', fontSize: '20px' }}>
+                    {animeInfo.status}
+                </p>
             </div>
-        </>
-
-    )
+        </div>
+    );
 }
